7c3324a7969a86f396875ee2e1d7afc4c258a02e
Amine Kammoun <mohamedkammoun2010@gmail.com>
2023-07-11 11:37:48 +0100

    second commit

---
+++ backend/src/main/java/com/yt/backend/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.yt.backend.repository;
+
+import com.yt.backend.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+}
---
+++ backend/src/main/java/com/yt/backend/service/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.yt.backend.service;
+
+import com.yt.backend.model.User;
+import com.yt.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService{
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(long id) {
+        return userRepository.findById(id).get() ;
+    }
+
+    @Override
+    public List<User> getUsers() {
+        return (List<User>) userRepository.findAll();
+    }
+
+    @Override
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User updateUser(User user) {
+return userRepository.save(user) ;   }
+}
