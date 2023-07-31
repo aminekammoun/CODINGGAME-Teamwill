4bba3ad5cafc216590b8c55ac586ee8174895388
azizbenzarti <azizbenzarti74@gmail.com>
2023-07-31 11:46:47 +0200

    Revert "Merge branch 'feature/aziz-dev' of https://github.com/aminekammoun/CODINGGAME-Teamwill into feature/aziz-dev"
    This reverts commit 37007d589078d5552e25d7de187e909a36ab2316, reversing
    changes made to 1cdc234b043d54475b5384ebf5a9e02a85a76749.

--- backend/src/main/java/com/yt/backend/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yt.backend.repository;
-
-import com.yt.backend.model.Question;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository  extends CrudRepository<Question,Long> {
-}
--- backend/src/main/java/com/yt/backend/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yt.backend.repository;
-
-import com.yt.backend.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User,Long> {
-}
